90ba8c23c3360b543584d6df60497b665e46c42e
Kiepi <44749733+Kiepi@users.noreply.github.com>
2021-05-04 13:09:02 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$db_user = "e14_szpital";
-$db_password = "e14_mama123@";
-$db_name =  "e14_szpital";
+$db_user = "";
+$db_password = "";
+$db_name =  "";
 
 $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 $polaczenie->set_charset("utf8");
